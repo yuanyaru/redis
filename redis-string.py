bfa776a1d00cf75009109d520a7fbf7dcb1c7dd7
yuanyaru <398916756@qq.com>
2019-09-06 09:41:41 +0800

    redis string

--- redis-string.py
+++ redis-string.py
@@ -76,9 +76,78 @@ print str(x2[0]), str(x2[1]).decode('utf-8')
 """
 设置新值并获取原来的值
 getset(name, value)
-"""
+
 r.set('name1', 'old')
 x = r.getset('name1', 'new')  # 获取原来的值
 y = r.get('name1')  # 获取新值
 print x
 print y
+"""
+"""
+getrange(key, start, end)
+获取子序列（根据字节获取，非字符）
+参数：
+  name，Redis 的 name
+  start，起始位置（字节）
+  end，结束位置（字节）
+如： "张三" ，0-3表示 "张"
+
+r.set('name1', '张三')
+r.set('name2', 'lucky')
+x = r.getrange('name1', 0, 2)
+y = r.getrange('name2', 0, 2)
+print(x, y)
+print(str(x, encoding='utf-8'))
+"""
+"""
+setrange(name, offset, value)
+修改字符串内容，从指定字符串索引开始向后替换（新值太长时，则向后添加）
+参数：
+    offset，字符串的索引，字节（一个汉字三个字节）
+    value，要设置的值
+
+r.set('name1', '张三')
+r.set('name2', 'lucky')
+r.setrange('name1', 3, '无忌')
+r.setrange('name2', 1, 'hello')
+x = r.get('name1')
+y = r.get('name2')
+print(x, y)
+print(str(x, encoding='utf-8'))
+"""
+# 返回name对应值的字节长度（一个汉字3个字节）
+r.mset({'name1': '里斯', 'name2': '张三'})
+x = r.strlen('name1')
+print(x)
+
+"""
+incr(self, name, amount=1)
+自增 name 对应的值，当 name 不存在时，则创建 name＝amount，否则，则自增。
+参数：
+    name,Redis的name
+    amount,自增数（必须是整数）
+"""
+r.incr('x1', 2)
+x1 = r.get('x1')
+print(x1)
+
+r.incrbyfloat('x2', 1.1)
+x2 = r.get('x2')
+print(x2)
+
+# 自减
+r.decr('x3', 1)
+x3 = r.get('x3')
+print(x3)
+
+"""
+append(key, value)
+ 在 redis name 对应的值后面追加内容
+参数：
+    key, redis的name
+    value, 要追加的字符串
+"""
+r.set('x1', 'hello')
+r.append('x1', ',world')
+x = r.get('x1')
+print(x)
