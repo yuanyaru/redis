e0b9c153aee498b25f6445263ea9ec755d145180
yuanyaru <398916756@qq.com>
2019-09-17 16:08:56 +0800

    update

--- redis-string.py
+++ redis-string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- encoding:utf-8 -*-
+# redis string 操作
 
 import redis
 import time
 
-# redis string 操作
 # 连接
 r = redis.Redis(host="192.168.100.64", port=6379)
 """
@@ -15,46 +15,32 @@ set(name, value, ex=None, px=None, nx=False, xx=False)
  px，过期时间（毫秒）
  nx，如果设置为 True，则只有 name 不存在时，当前 set 操作才执行
  xx，如果设置为 True，则只有 name 存在时，岗前 set 操作才执行
-
-r.set('name1', '张三', ex=10, nx=True)
+"""
+r.set("name1", "张三", ex=10, nx=True)
 x1 = r.get('name1')
-print str(x1).decode('utf-8')
-"""
-"""
-setnx(name, value)
-设置值，只有 name 不存在时，执行设置操作（添加）
+print(str(x1, encoding='utf-8'))
 
+# 只有 name 不存在时，执行设置操作（添加）
 r.set('name1', '张三', ex=5)
-r.setnx('name1', '李四')  # name1 存在，不执行，name1 中值仍然是‘张三’
-r.setnx('name2', '王五')  # name2 不存在，执行，name2 中值是‘王五’
+r.setnx('name1', '李四')  # name1 存在，不执行
+r.setnx('name2', '王五')  # name2 不存在，执行
 x2 = r.get('name1')
 x3 = r.get('name2')
-print str(x2).decode('utf-8')
-print str(x3).decode('utf-8')
-"""
-"""
-setex(name, value, time)
-设置值,可设置过期时间
-参数：
-    time，过期时间（数字秒 或 timedelta 对象）
+print(str(x2, encoding='utf-8'))
+print(str(x3, encoding='utf-8'))
 
-r.setex('name3', '李四', 2)
-time.sleep(3)
-x = r.get('name3')
-print str(x).decode('utf-8')
-"""
-"""
-psetex(name, time_ms, value)
-设置值
-参数：
-    time_ms，过期时间（数字毫秒 或 timedelta 对象）
+# 设置值,可设置过期时间
+# r.setex('name3', '李四', 3)
+# time.sleep(3)
+# x4 = r.get('name3')
+# print(str(x4, encoding='utf-8'))
 
-r.set('name4', '张三')
-r.psetex('name4', 3, '李四')
-time.sleep(0.001)
-x = r.get('name4')
-print str(x).decode('utf-8')
-"""
+# r.set('name4', '张三')
+# r.psetex('name4', 3, '李四')
+# time.sleep(0.001)
+# x5 = r.get('name4')
+# print(str(x5, encoding='utf-8'))
+
 """
 批量设置值 mset(*args, **kwargs)
 如：
@@ -64,48 +50,30 @@ print str(x).decode('utf-8')
 批量获取 mget(keys, *args)
 如：
     mget('ylr', 'Lily') 或 r.mget(['ylr', 'Lily'])
-
-r.mset({'name1': 'lisi', 'name2': '王五'})
-x = r.get('name1')
-x1 = r.mget('name1', 'name2')
-x2 = r.mget(['name1', 'name2'])
-print x, x1, x2
-print str(x1[0]), str(x1[1]).decode('utf-8')
-print str(x2[0]), str(x2[1]).decode('utf-8')
 """
-"""
-设置新值并获取原来的值
-getset(name, value)
+r.mset({'name5': 'lisi', 'name6': '王五'})
+x6 = r.get('name5')
+x7 = r.mget('name5', 'name6')
+x8 = r.mget(['name5', 'name6'])
+print(x6, x7, x8)
+print(str(x7[0]), str(x7[1], encoding='utf-8'))
+print(str(x8[0]), str(x8[1], encoding='utf-8'))
 
+# 设置新值并获取原来的值
 r.set('name1', 'old')
 x = r.getset('name1', 'new')  # 获取原来的值
 y = r.get('name1')  # 获取新值
-print x
-print y
-"""
-"""
-getrange(key, start, end)
-获取子序列（根据字节获取，非字符）
-参数：
-  name，Redis 的 name
-  start，起始位置（字节）
-  end，结束位置（字节）
-如： "张三" ，0-3表示 "张"
+print(x, y)
 
+# 获取子序列（根据字节获取，非字符） 如： "张三" ，0-3表示 "张" （一个汉字三个字节）
 r.set('name1', '张三')
 r.set('name2', 'lucky')
 x = r.getrange('name1', 0, 2)
 y = r.getrange('name2', 0, 2)
 print(x, y)
 print(str(x, encoding='utf-8'))
-"""
-"""
-setrange(name, offset, value)
-修改字符串内容，从指定字符串索引开始向后替换（新值太长时，则向后添加）
-参数：
-    offset，字符串的索引，字节（一个汉字三个字节）
-    value，要设置的值
 
+# 修改字符串内容，从指定字符串索引开始向后替换（新值太长时，则向后添加）
 r.set('name1', '张三')
 r.set('name2', 'lucky')
 r.setrange('name1', 3, '无忌')
@@ -114,39 +82,27 @@ x = r.get('name1')
 y = r.get('name2')
 print(x, y)
 print(str(x, encoding='utf-8'))
-"""
-# 返回name对应值的字节长度（一个汉字3个字节）
+
+# 返回 name 对应值的字节长度
 r.mset({'name1': '里斯', 'name2': '张三'})
 x = r.strlen('name1')
 print(x)
 
-"""
-incr(self, name, amount=1)
-自增 name 对应的值，当 name 不存在时，则创建 name＝amount，否则，则自增。
-参数：
-    name,Redis的name
-    amount,自增数（必须是整数）
-"""
-r.incr('x1', 2)
-x1 = r.get('x1')
-print(x1)
+# incr(self, name, amount=1) 自增 name 对应的值，当 name 不存在时，则创建 name＝amount，否则，则自增。
+r.incr('y1', 2)
+y1 = r.get('y1')
+print(y1)
 
-r.incrbyfloat('x2', 1.1)
-x2 = r.get('x2')
-print(x2)
+r.incrbyfloat('y2', 1.1)
+y2 = r.get('y2')
+print(y2)
 
 # 自减
-r.decr('x3', 1)
-x3 = r.get('x3')
-print(x3)
+r.decr('y3', 1)
+y3 = r.get('y3')
+print(y3)
 
-"""
-append(key, value)
- 在 redis name 对应的值后面追加内容
-参数：
-    key, redis的name
-    value, 要追加的字符串
-"""
+# 在 redis name 对应的值后面追加内容
 r.set('x1', 'hello')
 r.append('x1', ',world')
 x = r.get('x1')
