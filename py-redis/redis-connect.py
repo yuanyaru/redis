edf6ddc3d862725160eb764d90daf8391d8f113c
yuanyaru <398916756@qq.com>
2020-11-12 16:07:31 +0800

    update

--- py-redis/redis-connect.py
+++ py-redis/redis-connect.py
@@ -4,7 +4,7 @@
 import redis
 
 # redis 连接
-r = redis.Redis(host="192.168.100.63", port=6381)
+r = redis.Redis(host="192.168.100.63", port=8001)
 # r.set('hello', 'world')
-x = r.get('c')
+x = r.get('hello')
 print(x)
